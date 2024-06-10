b51753fe632c20bd7d1d6da14905c43caac0eb49
Daniel Morales <damesca@uma.es>
2024-06-10 10:13:51 +0200

    Modifications added to the ReEncryptionKey class to handle more constructors.

--- src/main/java/nics/crypto/ntrureencrypt/ReEncryptionKey.java
+++ src/main/java/nics/crypto/ntrureencrypt/ReEncryptionKey.java
@@ -5,7 +5,14 @@
 
 package nics.crypto.ntrureencrypt;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import net.sf.ntru.exception.NtruException;
 import net.sf.ntru.polynomial.IntegerPolynomial;
+import net.sf.ntru.util.ArrayEncoder;
 
 /**
  *
@@ -13,18 +20,41 @@ import net.sf.ntru.polynomial.IntegerPolynomial;
  */
 public class ReEncryptionKey {
 
-    
+    public int N;
+    public int q;
     public IntegerPolynomial rk;
     
-    
+    public ReEncryptionKey(byte[] b) {
+        this(new ByteArrayInputStream(b));
+    }
+
+    public ReEncryptionKey(InputStream is) {
+        DataInputStream dataStream = new DataInputStream(is);
+        try {
+            N = dataStream.readShort();
+            q = dataStream.readShort();
+            rk = IntegerPolynomial.fromBinary(dataStream, N, q);
+        } catch (IOException e) {
+            throw new NtruException(e);
+        }
+    }
 
     public ReEncryptionKey(IntegerPolynomial fA, IntegerPolynomial fB, int q) {
         
+        //if(fA.coeffs.length != fB.coeffs.length) {
+        //    throw new Exception("Selected private keys do not have the same length.");
+        //}
+
+        this.q = q;
+        this.N = fA.coeffs.length;
+
         IntegerPolynomial fBinv = fB.toIntegerPolynomial().invertFq(q);
-        
         rk = fA.toIntegerPolynomial().mult(fBinv);
         
+    }
 
+    public byte[] getEncoded() {
+        return ArrayEncoder.concatenate(ArrayEncoder.toByteArray(this.N), ArrayEncoder.toByteArray(this.q), rk.toBinary(this.q));
     }
     
 }
